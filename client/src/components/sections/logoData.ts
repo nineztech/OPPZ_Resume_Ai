cd6ebb8a59ce4b0227d65582d2c4953792b5ecfb
SakshiShah21 <shahsakshi02817@gmail.com>
2025-08-19 18:10:07 +0530

    ui  integrate
    done the ui part

---
+++ client/src/components/sections/logoData.ts
@@ -0,0 +1,56 @@
+export const logos = [
+  {
+    name: "Microsoft",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Google",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Amazon",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "IBM",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Oracle",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Intel",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Intel_logo_%282006-2020%29.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Cisco",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Cisco_logo_blue_2016.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "Salesforce",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg",
+    width: 120,
+    height: 40,
+  },
+  {
+    name: "VMware",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Vmware.svg",
+    width: 120,
+    height: 40,
+  },
+];
