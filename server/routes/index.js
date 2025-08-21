d3293c38f5147b1d0a116687931b2478afe4f3e9
SakshiShah21 <shahsakshi02817@gmail.com>
2025-08-21 18:34:08 +0530

    save backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,12 @@
 import userRoute from './userRoutes.js'
 import aiSuggestionRoute from './aiSuggestionRoutes.js'
-<<<<<<< Updated upstream
-
-=======
 // import resumeModel from '../models/resumeModel.js' 
 import resumeRoute from './resumeRoutes.js'; // ✅ Make sure the filename is correct
->>>>>>> Stashed changes
 import express from 'express'
 const router = express.Router()
 
 router.use("/user", userRoute)
 router.use("/ai", aiSuggestionRoute)
-<<<<<<< Updated upstream
-
-=======
 // router.use("/resume", resumeModel)
 router.use("/resume", resumeRoute)
->>>>>>> Stashed changes
 export default router
