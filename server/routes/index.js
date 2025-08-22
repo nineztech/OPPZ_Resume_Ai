3630dc99af7b0e5e09a59565ea0f507a24ede9e3
SakshiShah21 <shahsakshi02817@gmail.com>
2025-08-22 11:02:06 +0530

    done the backend api part

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,11 @@
 import userRoute from './userRoutes.js'
 import aiSuggestionRoute from './aiSuggestionRoutes.js'
-// import resumeModel from '../models/resumeModel.js' 
-import resumeRoute from './resumeRoutes.js'; // ✅ Make sure the filename is correct
+import resumeRoute from './resumeRoutes.js'; 
 import express from 'express'
 const router = express.Router()
 
 router.use("/user", userRoute)
 router.use("/ai", aiSuggestionRoute)
-// router.use("/resume", resumeModel)
 router.use("/resume", resumeRoute)
+
 export default router
